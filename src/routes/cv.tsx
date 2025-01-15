117e263f216d1ac43c0b102b87cced4a0d36e80a
Toma Rosa <toma.rosa@ik.me>
2025-01-15 14:48:25 +0100

    feat(cv): enhance CV component with additional projects and formations section

--- src/routes/cv.tsx
+++ src/routes/cv.tsx
@@ -1,6 +1,7 @@
 import Avatar from "@/assets/avatar.jpg";
 import { GithubIcon } from "@/assets/icons/github-icon";
 import LinkIcon from "@/assets/icons/link-icons";
+import LinkedinIcon from "@/assets/icons/linkedin-icon";
 import MailIcon from "@/assets/icons/mail-icon";
 import PhoneIcon from "@/assets/icons/phone-icon";
 import { createFileRoute } from "@tanstack/react-router";
@@ -15,48 +16,53 @@ function RouteComponent() {
       <div className="mx-auto w-[827px] space-y-3">
         <div className="flex gap-3">
           <PersonalInfos />
-          <About />
+          <div className="grid gap-3">
+            <About />
+            <Formations />
+          </div>
         </div>
-        <Interlude />
         <div className="grid grid-cols-2 gap-3">
-          <ProjectCard
+          <CvProjectCard
+            status="pro"
             title="Astradal Designer"
             tools="PostgreSQL, Postgis, ASP Net Core, React, Typescript, TailwindCSS, TanStack Router, OpenLayers"
             description="Application web de gestion de données géographiques. Création, édition et visualisation de cartes publique et privée, je peux vous faire une démonstration sur demande."
             websiteUrl="https://app.astradal.com/"
-            wip
           />
-          <ProjectCard
+          <CvProjectCard
+            status="open-source"
             title="Tanstack Form"
             tools="Typescript"
-            description="Contribution open source à la librairie TanStack Form. Ajout de fonctionnalités concernant la validation et la gestion des champs de type 'array'."
+            description="Contribution open source à la librairie TanStack Form. Ajout de plusieurs fonctionnalités concernant la validation, la gestion des champs de type 'array', etc."
             websiteUrl="https://tanstack.com/form/latest"
           />
-          <ProjectCard
+          <CvProjectCard
+            status="pro"
             title="Jane Do Agency"
             tools="Next.js, React, Typescript, TailwindCSS, Sanity"
             description="Site vitrine promotionnel de l'agence musicale Jane Do Agency. Interface et design sur mesure, gestion de contenu via CMS et prise de contact."
             websiteUrl="https://www-janedoagency-com.vercel.app/"
           />
-          <ProjectCard
-            wip
+          <CvProjectCard
+            status="perso"
             title="Ark Stack UI"
             tools="React, Typescript, TailwindCSS, Ark-ui"
             description="Implémentation d'une librairie de composants UI autour des composants de la librairie Ark-ui avec React, TypeScript et TailwindCSS."
             githubUrl="https=//github.com/t-rosa/ark-stack-ui"
           />
-          <ProjectCard
-            wip
-            title="Abalone"
-            tools="C++, Raylib"
-            description="Implémentation du jeu de plateau Abalone en C++ avec la librairie Raylib."
-            githubUrl="https://github.com/t-rosa/ABALONE"
-          />
-          <ProjectCard
+          <CvProjectCard
+            status="perso"
             title="Better League Client"
             tools="Astro, TailwindCSS"
             description="Ma conception du client de League of legends s'il devait être refait. Principalement du CSS et des animations."
             websiteUrl="https://better-league-client.vercel.app/"
+          />
+          <CvProjectCard
+            status="perso"
+            title="Abalone"
+            tools="C++, Raylib"
+            description="Implémentation du jeu de plateau Abalone en C++ avec la librairie Raylib."
+            githubUrl="https://github.com/t-rosa/ABALONE"
           />
         </div>
       </div>
@@ -66,42 +72,50 @@ function RouteComponent() {
 
 function PersonalInfos() {
   return (
-    <div className="border rounded-xl border-zinc-300/75 p-6 space-y-3 w-md">
+    <div className="border rounded-xl border-zinc-300/75 p-6 space-y-3 w-lg ">
       <img
         src={Avatar}
         className="rounded-full size-25 border border-zinc-300/75"
       />
-      <h1 className="text-2xl text-indigo-400">
-        Hey, I'm <span className="font-semibold">Toma</span>.
-      </h1>
+      <h1 className="text-2xl font-semibold text-indigo-400">Toma ROSA</h1>
       <div className="space-y-1">
         <p className="text-zinc-600 text-lg">
           Ingénieur logiciel, spécialisé dans le web.
         </p>
-        <ul className="text-zinc-500 text-sm space-y-1">
-          <li className="flex items-center gap-1">
-            <LinkIcon className="size-4" /> t-rosa.com
-          </li>
-          <li className="flex items-center gap-1">
-            <MailIcon className="size-4" /> toma.rosa@ik.me
-          </li>
-          <li className="flex items-center gap-1">
-            <PhoneIcon className="size-4" /> 06 41 77 01 36
-          </li>
-        </ul>
       </div>
-    </div>
-  );
-}
-
-function Interlude() {
-  return (
-    <div className="border rounded-xl border-zinc-300/75 p-3 space-y-3">
-      <p className="text-zinc-600 text-pretty ">
-        Passionné et motivé, je suis ouvert à toutes propositions pour un poste
-        de développeur web, quelle que soit la technologie. N&apos;hésitez pas à
-        me contacter ou à visiter mon site web. Merci !
-      </p>
+      <ul className="text-zinc-500 text-sm space-y-1">
+        <li>
+          <a href="https://t-rosa.com" className="flex items-center gap-1">
+            <LinkIcon className="size-4" /> t-rosa.com
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/t-rosa"
+            className="flex items-center gap-1"
+          >
+            <GithubIcon className="size-4" /> github.com/t-rosa
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://wwww.linkedin.com/in/toma-rosa/"
+            className="flex items-center gap-1"
+          >
+            <LinkedinIcon className="size-4" /> linkedin.com/in/toma-rosa/
+          </a>
+        </li>
+        <li>
+          <a href="mailto:toma.rosa@ik.me" className="flex items-center gap-1">
+            <MailIcon className="size-4" /> toma.rosa@ik.me
+          </a>
+        </li>
+        <li className="flex items-center gap-1">
+          <a href="tel:0641770136" className="flex items-center gap-1">
+            <PhoneIcon className="size-4" /> 06 41 77 01 36
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
@@ -111,26 +125,9 @@ function About() {
     <div className="border rounded-xl border-zinc-300/75 p-6 space-y-3">
       <h1 className="text-2xl text-indigo-400">À propos</h1>
       <p className="text-zinc-600">
-        Je suis un titulaire d&apos;un Master 2 avec une spécialisation en
-        développement web et je travaille depuis 5 ans au sein de la
-        société&nbsp;
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          href="https://www.astradal.com/"
-          className="underline"
-        >
-          Astradal
-        </a>
-        .
-      </p>
-      <p className="text-zinc-600">
-        Pendant mon temps libre, j&apos;aime implémenter des jeux avec Raylib,
-        contribuer à des projets open source et essayer de nouvelles choses.
-      </p>
-      <p className="text-zinc-600">
-        En dehors de la programmation, j&apos;aime jouer à Street Fighter, aux
-        jeux de cartes et lire les romans Warcraft.
+        Développeur web, 5 ans d&apos;expériences, maîtrise de TypeScript,
+        React, C#, ASP.NET Core, PostgreSQL, Docker et d&apos;autres, je suis
+        prêt et motivé à contribuer efficacement à vos projets.
       </p>
     </div>
   );
@@ -142,15 +139,25 @@ interface Props {
   description: string;
   githubUrl?: string;
   websiteUrl?: string;
-  wip?: boolean;
+  status?: "pro" | "perso" | "open-source";
 }
 
-export function ProjectCard(props: Props) {
+function CvProjectCard(props: Props) {
   return (
     <div className="relative border rounded-xl border-zinc-300/75 p-3 lg:p-6 space-y-3 grid grid-rows-[auto_1fr_auto] gap-3">
-      {props.wip && (
-        <p className="absolute z-10 -top-2 right-10 border px-3 rounded-xl bg-zinc-100 border-zinc-300/75 text-zinc-600 text-xs">
-          work in progress
+      {props.status === "pro" && (
+        <p className="absolute z-10 -top-2 right-7 border px-3 rounded-xl bg-zinc-100 border-zinc-300/75 text-zinc-600 text-xs">
+          pro
+        </p>
+      )}
+      {props.status === "perso" && (
+        <p className="absolute z-10 -top-2 right-7 border px-3 rounded-xl bg-zinc-100 border-zinc-300/75 text-zinc-600 text-xs">
+          perso
+        </p>
+      )}
+      {props.status === "open-source" && (
+        <p className="absolute z-10 -top-2 right-7 border px-3 rounded-xl bg-zinc-100 border-zinc-300/75 text-zinc-600 text-xs">
+          open source
         </p>
       )}
       <div className="space-y-1">
@@ -185,6 +192,48 @@ export function ProjectCard(props: Props) {
             </a>
           </li>
         )}
+      </ul>
+    </div>
+  );
+}
+
+function Formations() {
+  return (
+    <div className="border rounded-xl border-zinc-300/75 p-6 space-y-3">
+      <h1 className="text-2xl text-indigo-400">Formations</h1>
+      <ul className="list-disc pl-6">
+        <li>
+          <a
+            href="https://www.francecompetences.fr/recherche/rncp/35078/"
+            className="text-indigo-500 underline"
+          >
+            M2 - Expert Développement Web - Ynov Toulouse
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.francecompetences.fr/recherche/rncp/30714/"
+            className="text-indigo-500 underline"
+          >
+            Bachelore CDAN - IPI Toulouse
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.francecompetences.fr/recherche/rncp/27812/"
+            className="text-indigo-500 underline"
+          >
+            Bachelore CSIA - IFC Avignon
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.francecompetences.fr/recherche/rncp/35340/"
+            className="text-indigo-500 underline"
+          >
+            BTS SIO - Lycée Aubanel Avignon
+          </a>
+        </li>
       </ul>
     </div>
   );
