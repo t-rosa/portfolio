eec7490e51809b4aadf8aeeefe831a027811be0f
Toma Rosa <toma.rosa@ik.me>
2025-01-15 09:07:04 +0100

    fix(layout): adjust padding in layout component for improved spacing

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,7 @@ import { PersonalCard } from "./personal-card";
 function Layout({ children }: React.PropsWithChildren) {
   return (
     <div className="min-h-dvh radial">
-      <div className="mx-auto max-w-7xl py-4 lg:py-24 px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl py-4 lg:py-12 px-4 sm:px-6 lg:px-8">
         <div className="mx-auto max-w-5xl">
           <Header />
           <main className="space-y-6 py-6">
