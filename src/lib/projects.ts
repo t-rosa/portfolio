0eee0e598d26e79b36f2fd2d7c5bdf0162369c80
Toma Rosa <toma.rosa@ik.me>
2025-01-16 15:03:20 +0100

    feat(projects): update project details and add new entries for improved portfolio representation

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -14,20 +14,20 @@ export const projects: Project[] = [
     tools:
       "PostgreSQL, Postgis, ASP Net Core, React, Typescript, TailwindCSS, TanStack Router, OpenLayers",
     description:
-      "Application web de gestion de données géographiques. Création, édition et visualisation de cartes publique et privée, je peux vous faire une démonstration sur demande.",
+      "Editeur de carte en ligne. Création, édition et visualisation de cartes publique et privée, je peux vous faire une démonstration sur demande.",
     websiteUrl: "https://app.astradal.com/",
     status: "pro",
     company: "Astradal",
   },
   {
-    title: "Jane Do Agency",
-    tools: "Next.js, React, Typescript, TailwindCSS, Sanity",
+    title: "Geostack",
+    tools:
+      "PostgreSQL, Postgis, ASP Net Core, React, Typescript, TailwindCSS, TanStack Router, OpenLayers",
     description:
-      "Site vitrine promotionnel de l'agence musicale Jane Do Agency. Interface et design sur mesure, gestion de contenu via CMS et prise de contact.",
+      "Application de gestion de données géographiques, avec divers outils de personnalisation de cartes mais aussi d'objets géographiques et de métiers associés.",
     websiteUrl: "https://www-janedoagency-com.vercel.app/",
     githubUrl: "https://github.com/t-rosa/www.janedoagency.com",
-    status: "pro",
-    company: "Jane Do",
+    status: "perso",
   },
   {
     title: "Tanstack Form",
@@ -40,21 +40,11 @@ export const projects: Project[] = [
     status: "open-source",
   },
   {
-    title: "Maison de la mascotte",
-    tools: "Astro, TailwindCSS",
+    title: "Ark Stack UI",
+    tools: "React, Typescript, TailwindCSS, Ark-ui",
     description:
-      "Site vitrine pour une maison d'hôtes. Présentation de la maison, services et informations de contact.",
-    githubUrl: "https://github.com/t-rosa/www.maisondelamascotte.com",
-    websiteUrl: "https://www.maisondelamascotte.com/",
-    status: "pro",
-  },
-  {
-    title: "Better League Client",
-    tools: "Astro, TailwindCSS",
-    description:
-      "Ma conception du client de League of legends s'il devait être refait. Principalement du CSS et des animations.",
-    githubUrl: "https://github.com/t-rosa/better-league-client",
-    websiteUrl: "https://better-league-client.vercel.app/",
+      "Implémentation d'une librairie de composants UI autour des composants de la librairie Ark-ui avec React, TypeScript et TailwindCSS.",
+    githubUrl: "https://github.com/t-rosa/ark-stack-ui",
     status: "perso",
   },
   {
@@ -66,12 +56,22 @@ export const projects: Project[] = [
     status: "perso",
   },
   {
-    title: "Ark Stack UI",
-    tools: "React, Typescript, TailwindCSS, Ark-ui",
+    title: "Better League Client",
+    tools: "Astro, TailwindCSS",
     description:
-      "Implémentation d'une librairie de composants UI autour des composants de la librairie Ark-ui avec React, TypeScript et TailwindCSS.",
-    githubUrl: "https://github.com/t-rosa/ark-stack-ui",
+      "Ma conception du client de League of legends s'il devait être refait. C'est mon terrain de jeu pour faire des animations et du CSS.",
+    githubUrl: "https://github.com/t-rosa/better-league-client",
+    websiteUrl: "https://better-league-client.vercel.app/",
     status: "perso",
+  },
+  {
+    title: "Maison de la mascotte",
+    tools: "Astro, TailwindCSS",
+    description:
+      "Site vitrine pour une maison d'hôtes. Présentation de la maison, services et informations de contact.",
+    githubUrl: "https://github.com/t-rosa/www.maisondelamascotte.com",
+    websiteUrl: "https://www.maisondelamascotte.com/",
+    status: "pro",
   },
   {
     title: "Abalone",
